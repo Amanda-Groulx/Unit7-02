4470ced221425c54fa6105d12c5d12faa654e8bd
AmandaGroulx <>
2021-05-08 19:40:26 +0000

    added space and took away space, fixed doublequote

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 document.getElementById('button').addEventListener('click', comparison)
 let userInput = 0
 
-function comparison() {
-  // Get user input and convert to integer 
-  userInput = document.getElementById('input').value  
+function comparison () {
+  // Get user input and convert to integer
+  userInput = document.getElementById('input').value
   userInput = parseInt(userInput)
 
-  //check if user input is less than 0
+  // Check if user input is less than 0
   if (userInput < 0) {
-    document.getElementById('answer').innerHTML = "Negative"
+    document.getElementById('answer').innerHTML = 'Negative'
   } else {
-    document.getElementById('answer').innerHTML = "Positive"
-  } 
+    document.getElementById('answer').innerHTML = 'Positive'
+  }
 }
